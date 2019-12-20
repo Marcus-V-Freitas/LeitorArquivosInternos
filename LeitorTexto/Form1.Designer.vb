060c9263ae02a231db562bcf34ec88cb4f088e0c
Marcus-V-Freitas <marcusviniciusdefreitascosta@yahoo.com.br>
2019-12-19 21:37:33 -0300

    inicial

---
+++ LeitorTexto/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtArquivo = New System.Windows.Forms.TextBox()
+        Me.btnCarregar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtProcurar = New System.Windows.Forms.TextBox()
+        Me.btnProcurar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtArquivo
+        '
+        Me.txtArquivo.Location = New System.Drawing.Point(12, 96)
+        Me.txtArquivo.Multiline = True
+        Me.txtArquivo.Name = "txtArquivo"
+        Me.txtArquivo.Size = New System.Drawing.Size(776, 551)
+        Me.txtArquivo.TabIndex = 0
+        '
+        'btnCarregar
+        '
+        Me.btnCarregar.Location = New System.Drawing.Point(33, 34)
+        Me.btnCarregar.Name = "btnCarregar"
+        Me.btnCarregar.Size = New System.Drawing.Size(127, 25)
+        Me.btnCarregar.TabIndex = 1
+        Me.btnCarregar.Text = "Carregar"
+        Me.btnCarregar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(412, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Caminho:"
+        '
+        'txtProcurar
+        '
+        Me.txtProcurar.Location = New System.Drawing.Point(465, 34)
+        Me.txtProcurar.Name = "txtProcurar"
+        Me.txtProcurar.Size = New System.Drawing.Size(237, 20)
+        Me.txtProcurar.TabIndex = 3
+        '
+        'btnProcurar
+        '
+        Me.btnProcurar.Location = New System.Drawing.Point(708, 31)
+        Me.btnProcurar.Name = "btnProcurar"
+        Me.btnProcurar.Size = New System.Drawing.Size(90, 25)
+        Me.btnProcurar.TabIndex = 4
+        Me.btnProcurar.Text = "Procurar"
+        Me.btnProcurar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(808, 659)
+        Me.Controls.Add(Me.btnProcurar)
+        Me.Controls.Add(Me.txtProcurar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCarregar)
+        Me.Controls.Add(Me.txtArquivo)
+        Me.Name = "Form1"
+        Me.Text = "LeitorTexto"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtArquivo As TextBox
+    Friend WithEvents btnCarregar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtProcurar As TextBox
+    Friend WithEvents btnProcurar As Button
+End Class
---
+++ LeitorTexto/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Private Sub btnCarregar_Click(sender As Object, e As EventArgs) Handles btnCarregar.Click
+
+        'Abre caixa de dialogo para selecionar o arquivo
+        Dim file As OpenFileDialog = New OpenFileDialog
+        Dim result As DialogResult = file.ShowDialog
+
+        'Armazena o nome do arquivo
+        Dim filename As String
+        filename = file.FileName
+
+        'Cria o Stream para ler o arquivo selecionado
+        Dim stream As System.IO.StreamReader
+        stream = New System.IO.StreamReader(filename)
+
+        'Mostra o texto obtido pelo stream até o fim
+        txtArquivo.Text = stream.ReadToEnd
+
+    End Sub
+
+    Private Sub btnProcurar_Click(sender As Object, e As EventArgs) Handles btnProcurar.Click
+
+        Dim position As Integer
+        Dim length As Integer
+        Dim text As String
+        Dim textFind As String
+
+        'Guarda o que estiver no txtArquivo na variavel text
+        text = txtArquivo.Text
+
+        'Guarda o valor desejado de busca na variavel textFind
+        textFind = txtProcurar.Text
+
+        'Pega a posição do valor desejado no text
+        position = text.IndexOf(textFind)
+
+        'Pega o tamanho do valor desejado
+        length = textFind.Length
+
+        'Checa se o valor desejado existe com base na posição e tamanho passados e dando um foco caso verdadeiro e uma mensagem de aviso em falso
+        If position > -1 Then
+            txtArquivo.Select(position, length)
+            txtArquivo.Focus()
+        Else
+            MessageBox.Show("Message not found")
+        End If
+
+    End Sub
+End Class
